3e671592d8628ff0b73e3b08252bd669e471dc9c
white-grudov <danekbel.1@gmail.com>
2023-05-24 11:39:47 +0300

    Updated Task.h

--- src/include/Task.h
+++ src/include/Task.h
@@ -12,77 +12,78 @@ struct Task
     using promise_type = task_promise<T>;
     using handle_type = std::coroutine_handle<promise_type>;
 
-    mutable handle_type m_handle;
+    mutable handle_type handle;
 
-    Task(handle_type handle)
-        : m_handle(handle) 
-    {}
+    Task(handle_type h) : handle { h } {}
 
-    Task(Task&& other) noexcept 
-        : m_handle(other.m_handle)
-    { other.m_handle = nullptr; };
+    Task(Task&& other) noexcept : handle { std::exchange(other.handle, nullptr) } {}
 
-    bool await_ready() 
-    { return m_handle.done(); }
+    bool await_ready() noexcept { return handle.done(); }
 
-    bool await_suspend(std::coroutine_handle<> handle) 
+    bool await_suspend(std::coroutine_handle<> h) noexcept
     {
-        if (!m_handle.done()) {
-            m_handle.resume();
+        if (!handle.done())
+        {
+            handle.resume();
         }
 
         return false;
     }
 
-    auto await_resume() 
-    { return get(); }
+    auto await_resume() { return get(); }
 
     T get() const 
     {     
-        if (!m_handle.done())
-            m_handle.resume();  
+        if (!handle.done())
+        {
+            handle.resume();  
+        }
+        auto& promise = handle.promise();
 
-        if (m_handle.promise().m_exception)
-            std::rethrow_exception(m_handle.promise().m_exception);
-
-        return *m_handle.promise().m_value;
+        if (promise.exception)
+        {
+            std::rethrow_exception(promise.exception);
+        }
+        return std::move(*promise.value);
     }
 
     ~Task() 
     {
-        if (m_handle)
-            m_handle.destroy();
+        if (handle)
+        {
+            handle.destroy();
+        }
     }
 
     template<typename U>
     struct task_promise 
     {
-        std::optional<T>    m_value {};
-        std::exception_ptr  m_exception = nullptr;
+        std::optional<T> value;
+        std::exception_ptr exception;
 
-        auto initial_suspend() 
-        { return std::suspend_always{}; }
+        auto initial_suspend() noexcept { return std::suspend_always{}; }
 
-        auto final_suspend() noexcept
-        { return std::suspend_always{}; }
+        auto final_suspend() noexcept { return std::suspend_always{}; }
 
-        auto return_value(T t) 
+        auto return_value(T t) noexcept
         {
-            m_value = t;
+            value = std::move(t);
             return std::suspend_always{};
         }
 
-        Task<T> get_return_object()
-        { return {handle_type::from_promise(*this)}; }
+        Task<T> get_return_object() noexcept { return {handle_type::from_promise(*this)}; }
 
-        void unhandled_exception() 
-        {  m_exception = std::current_exception(); }
+        void unhandled_exception() noexcept
+        {
+            exception = std::current_exception();
+        }
 
         void rethrow_if_unhandled_exception()
         {
-            if (m_exception)
-                std::rethrow_exception(std::move(m_exception));
+            if (exception)
+            {
+                std::rethrow_exception(std::move(exception));
+            }
         }
     };
-
 };
